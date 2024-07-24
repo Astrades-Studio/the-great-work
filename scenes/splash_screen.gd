c775c247626d79ea8395e719d529d9ba12fde144
Falkon <andrex.93.p@gmail.com>
2024-07-24 14:32:59 -0300

    Sounds added

--- scenes/splash_screen.gd
+++ scenes/splash_screen.gd
@@ -5,8 +5,10 @@ const INTRO_ASTRA = preload("res://assets/sounds/music/intro astra.mp3")
 
 func _ready() -> void:
 	animation_player.play("fade in")
-	#audio_stream_player.play(0.0)
-	MusicManager.play_music(INTRO_ASTRA)
+	audio_stream_player.stream = INTRO_ASTRA
+	audio_stream_player.volume_db = -10.0  # Ajusta el volumen a -10 dB, puedes cambiar este valor
+	audio_stream_player.play()
+	# MusicManager.play_music(INTRO_ASTRA) # Comenta o elimina esta línea si no es necesaria
 
 func go_title_screen():
 	get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
@@ -15,7 +17,5 @@ func _input(event):
 	if(event is InputEventKey) or (event is InputEventMouseButton):
 		animation_player.speed_scale = 3.0
 		
- 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	# Replace with function body.
-		go_title_screen()
+	go_title_screen()
