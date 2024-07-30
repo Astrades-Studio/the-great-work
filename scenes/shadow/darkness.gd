af112f752b142d1f6f61af7d26d3adce70ddac82
Ignawesome <ignacio.parentella@gmail.com>
2024-07-30 19:30:17 -0300

    book names

--- scenes/shadow/darkness.gd
+++ scenes/shadow/darkness.gd
@@ -122,7 +122,7 @@ func play_random_whisper() -> void:
 	
 	# choose a DialogPiece at random from WHISPERS_DIALOG
 	var index = randi() % dialog.dialog.size()
-	var piece = dialog[index]
+	var piece = dialog.dialog[index]
 	DialogManager.create_subtitles_piece(piece)
 	play_random_shadow_sound()
 	# remove that piece from the dialog
