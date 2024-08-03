86f5b4dc48a9b1736ce571a3a5e9e9e13dd5fcd1
Ignawesome <ignacio.parentella@gmail.com>
2024-08-03 20:00:11 -0300

    flare fix

--- scenes/clock_bells.gd
+++ scenes/clock_bells.gd
@@ -5,8 +5,9 @@ extends Area3D
 var played : bool = false
 
 func _on_body_entered(body: Node3D) -> void:
+	if played:
+		return
 	if body is Player and GameManager.alchemy_recipe_read:
 		clock_bells.play(6.0)
-		if !played:
-			played = true
+		played = true
 			
