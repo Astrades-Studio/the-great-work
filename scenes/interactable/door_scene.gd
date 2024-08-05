37429b7201c56f179bef95bc7f88fc797c402dbb
Falkon <andrex.93.p@gmail.com>
2024-08-05 13:30:57 -0300

    Door sound

--- scenes/interactable/door_scene.gd
+++ scenes/interactable/door_scene.gd
@@ -3,6 +3,9 @@ extends StaticBody3D
 
 @export var door_id : StringName
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var door_opening: AudioStreamPlayer3D = $DoorOpening
+@onready var door_closing: AudioStreamPlayer3D = $DoorClosing
+@onready var door_locked: AudioStreamPlayer3D = $DoorLocked
 
 @export var locked = true
 var open = false
@@ -13,18 +16,25 @@ func _ready() -> void:
 		self.connect("interacted", Callable(self, "open_door"))
 		
 var tween : Tween
+		
+	#LOCKED DOOR
 func open_door():
 	if locked:
+		door_locked.play()
 		DialogManager.create_dialog_piece("It's locked")
 		return
 	
+	#OPENING DOOR
 	tween = get_tree().create_tween()
 	if !locked and !open:
 		#tween.tween_property(self, "rotation:y", 0.0, 1.0)
+		door_opening.play()
 		animation_player.play("open")
 		open = true
 		
+	#CLOSING DOOR
 	elif !locked and open:
 		#tween.tween_property(self, "rotation:y", 1.0, 1.0)
+		door_closing.play()
 		animation_player.play_backwards("open")
 		open = false
